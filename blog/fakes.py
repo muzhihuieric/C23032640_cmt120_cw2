8dad7a03637dac4bb8fe164857ff25a4ea49846b
nuzhi <muzhihuieric@gmail.com>
2024-01-08 21:45:41 +0000

    Initial commit

---
+++ blog/fakes.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+from faker import Faker
+from sqlalchemy.exc import IntegrityError
+
+from blog.extensions import db
+from blog.models import Admin, Category, Post, Comment, Link
+
+fake = Faker()
+
+
+def fake_admin():
+    admin = Admin(
+        username='admin',
+        blog_title='Blog',
+        blog_sub_title="There's nothing I want to say",
+        name='Personal blog',
+        about='Welcome to my personal blog'
+    )
+    admin.set_password('123456')
+    db.session.add(admin)
+    db.session.commit()
+
+
+def fake_categories(count=10):
+    category = Category(name='Default')
+    db.session.add(category)
+
+    for i in range(count):
+        category = Category(name=fake.word())
+        db.session.add(category)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+
+
+def fake_posts(count=50):
+    for i in range(count):
+        post = Post(
+            title=fake.sentence(),
+            body=fake.text(2000),
+            category=Category.query.get(random.randint(1, Category.query.count())),
+            timestamp=fake.date_time_this_year()
+        )
+
+        db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=500):
+    sites = ['https://twitter.com/elonmusk', 'https://twitter.com/haydaypop', 'https://twitter.com/hayday']
+    for i in range(count):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=random.choice(sites),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+    salt = int(count * 0.1)
+    for i in range(salt):
+        # unreviewed comments
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=False,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+        # from admin
+        comment = Comment(
+            author='Mima Kirigoe',
+            email='mima@example.com',
+            site='example.com',
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+    # replies
+    for i in range(salt):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            replied=Comment.query.get(random.randint(1, Comment.query.count())),
+            post=Post.query.get(random.randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+
+def fake_links():
+    twitter = Link(name='Twitter', url='https://www.twitter.com/')
+    facebook = Link(name='Facebook', url='https://www.facebook.com/')
+    linkedin = Link(name='LinkedIn', url='https://www.linkedin.com/')
+    db.session.add_all([twitter, facebook, linkedin])
+    db.session.commit()
---
+++ blog/settings.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+prefix = 'sqlite:///'
+
+
+class BaseConfig(object):
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev key')
+
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
+    CKEDITOR_ENABLE_CSRF = True
+    CKEDITOR_FILE_UPLOADER = 'admin.upload_image'
+
+    POST_PER_PAGE = 10
+    MANAGE_POST_PER_PAGE = 15
+    COMMENT_PER_PAGE = 15
+    # ('theme name', 'display name')
+    THEMES = {'perfect_blue': 'Perfect Blue', 'black_swan': 'Black Swan'}
+    SLOW_QUERY_THRESHOLD = 1
+
+    UPLOAD_PATH = os.path.join(basedir, 'uploads')
+    ALLOWED_IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = prefix + 'data.db'
+    print(SQLALCHEMY_DATABASE_URI)
+
+
+config = {
+    'production': ProductionConfig
+}
